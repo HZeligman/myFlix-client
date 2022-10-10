3263d90c4080d920f4ab447f75437cb01e07b605
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-10-10 14:40:41 -0400

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -28,14 +28,14 @@ export function RegistrationView(props) {
       setValues({ ...values, usernameErr: 'Username Required' });
       isReq = false;
     } else if (username.length < 5) {
-      setValues({ ...values, usernameErr: 'Username must be 5 characters long.' });
+      setValues({ ...values, usernameErr: 'Username must be 5 characters long.', });
       isReq = false;
     }
     if (!password) {
       setValues({ ...values, passwordErr: 'Password Required' });
       isReq = false;
     } else if (password.length < 6) {
-      setValues({ ...values, passwordErr: 'Password must be 6 characters long.' })
+      setValues({ ...values, passwordErr: 'Password must be 6 characters long.', });
       isReq = false;
     }
     if (!email) {
@@ -60,13 +60,13 @@ export function RegistrationView(props) {
         Email: email,
         Birthday: birthday
       })
-        .then(response => {
+        .then((response) => {
           const data = response.data;
           console.log(data);
           alert('Registration successful, please login!');
           window.open('/', '_self');
         })
-        .catch(response => {
+        .catch((response) => {
           console.error(response);
           alert('unable to register');
         });
@@ -81,31 +81,31 @@ export function RegistrationView(props) {
           <p></p>
           <Form.Group controlId="formUsername" className="reg-form-inputs">
             <Form.Label>Username:</Form.Label>
-            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
             {values.usernameErr && <p>{values.usernameErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="formName" className="reg-form-inputs">
             <Form.Label>Name:</Form.Label>
-            <Form.Control type="text" value={name} onChange={e => setName(e.target.value)} />
+            <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} />
             {values.nameErr && <p>{values.nameErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="formPassword" className="reg-form-inputs">
             <Form.Label>Password:</Form.Label>
-            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             {values.passwordErr && <p>{values.passwordErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="Email" className="reg-form-inputs">
             <Form.Label>Email:</Form.Label>
-            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             {values.emailErr && <p>{values.emailErr}</p>}
           </Form.Group>
 
           <Form.Group controlId="updateBirthday">
-            <Form.Label>Birthday</Form.Label>
-            <Form.Control type="date" name="birthday" onChange={e => setBirthday(e.target.value)}></Form.Control>
+            <Form.Label>Birthday:</Form.Label>
+            <Form.Control type="date" name="birthday" onChange={(e) => setBirthday(e.target.value)} />
           </Form.Group>
 
           <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
