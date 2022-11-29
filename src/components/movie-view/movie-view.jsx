9222b68b005062a3595118693db3af2f95cd0ceb
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-11-28 20:24:07 -0500

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./movie-view.scss"
+
 export class MovieView extends React.Component {
   render() {
     const { movie } = this.props;
@@ -7,7 +9,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src="{movie.ImagePath}" />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
