dfa9178dc67de776409314e8be12160437c4d9d4
HZeligman <103866815+HZeligman@users.noreply.github.com>
2022-11-29 21:46:26 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { MainView } from './components/main-view/main-view';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return <MainView />;
+    return (
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
+    );
   }
 }
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
-const root = ReactDOM.createRoot(container);
 
-// Tells React to render your app in the root DOM element
-root.render(React.createElement(MyFlixApplication));
+ReactDOM.render(React.createElement(MyFlixApplication), container);
